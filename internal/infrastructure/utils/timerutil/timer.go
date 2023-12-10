772826426e1f3c6d78402473abdff44fab9809e7
liningning <771846387@qq.com>
2023-12-10 23:05:49 +0800

    fix: 修复计时器阻塞

--- internal/infrastructure/utils/timerutil/timer.go
+++ internal/infrastructure/utils/timerutil/timer.go
@@ -4,10 +4,10 @@ import "time"
 
 func RunPeriodTask(f func(), duration time.Duration) {
 	ticker := time.NewTicker(duration)
-	defer ticker.Stop()
-
-	f()
-	for range ticker.C {
-		f()
-	}
+	go f()
+	go func() {
+		for range ticker.C {
+			f()
+		}
+	}()
 }
